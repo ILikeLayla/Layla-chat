e7b7517ddfeb2fcc4e4637ca0578d752a4c5d35f
Layla <chen_rui_tao0525@163.com>
2024-02-14 23:54:49 +0800

    1

--- client_core/src/config.rs
+++ client_core/src/config.rs
@@ -1,3 +1,3 @@
 pub const UNACCEPTABLE_NAME_CHAR: [&str; 3] = [",", "<", ">"];
-pub const ID_PLACEHOLDER: &str = "1";
+// pub const ID_PLACEHOLDER: &str = "1";
 // pub const FORCE_TO_USE_SYSTEM_TIME: bool = true;
--- client_core/src/manager.rs
+++ client_core/src/manager.rs
@@ -1,5 +1,5 @@
 use crate::{exception::{Error, ErrorStruct, Warn, WarnType}, User};
-use super::{group::Group, config::ID_PLACEHOLDER};
+use super::group::Group;
 use std::collections::HashMap;
 use std::sync::Arc;
 
@@ -16,15 +16,14 @@ impl Manager {
         }
     }
 
-    pub fn new_user_by_string(&mut self, s: String) -> Result<(), Error>{
-        let mut user: ErrorStruct<User> = s.into();
-        let mut current_id = user.get_id();
-        while self.user_list.contains_key(&current_id) {
-            current_id += ID_PLACEHOLDER
+    pub fn new_remote_user(&mut self, s: String) -> ErrorStruct<()> {
+        let user: ErrorStruct<User> = s.into();
+        let mut user = match user {
+            ErrorStruct::Ok(u) => u,
+            ErrorStruct::Err(e) => return ErrorStruct::Err(e)
         };
-        user.change_id(current_id);
-        let _ = self.user_list.insert(current_id, user);
-        Ok(())
+        let _ = self.user_list.insert(user.get_id(), user);
+        ErrorStruct::Ok(())
     }
 
     pub fn add_user_to_group(&mut self, user_id:String, group_id:String) -> Result<(), Warn> {
